b617cd77f9fd17e096c6bfb9f050b3adf8e3c9ad
tilman <tilman.fischer@prijanto.com>
2025-02-18 09:31:40 +0100

    adapt for 3 thermoelements

--- AluHotEnd_Controller.ino
+++ AluHotEnd_Controller.ino
@@ -3,12 +3,16 @@
 
 #include "max6675.h"
 
-int thermoDO = 4;
-int thermoCS = 5;
+int thermoDO = 5;
 int thermoCLK = 6;
 
-MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+int thermoCS1 = 2;
+int thermoCS2 = 3;
+int thermoCS3 = 4;
 
+MAX6675 thermocouple1(thermoCLK, thermoCS1, thermoDO);
+MAX6675 thermocouple2(thermoCLK, thermoCS2, thermoDO);
+MAX6675 thermocouple3(thermoCLK, thermoCS3, thermoDO);
 
 void setup() {
   Serial.begin(115200);
@@ -22,7 +26,7 @@ void loop() {
   // basic readout test, just print the current temp
   
   Serial.print("C = "); 
-  Serial.println(thermocouple.readCelsius());
+  Serial.println(thermocouple1.readCelsius());
  
   // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
   delay(1000);
